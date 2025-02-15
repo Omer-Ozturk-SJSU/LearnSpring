c58e2d307098d7bfb2849afdf043b3d416a52b5c
TIM AoX <degisikoyuncu1461@gmail.com>
2025-02-15 07:57:24 -0800

    First launch + Dependencies

--- src/main/java/com/example/LearnSpring/LearnSpringApplication.java
+++ src/main/java/com/example/LearnSpring/LearnSpringApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnSpringApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+
 		SpringApplication.run(LearnSpringApplication.class, args);
 	}
 
